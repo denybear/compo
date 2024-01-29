d900e83a4cff33f0b0513d7ac9fd86e3251d80fe
denybear <denybear@free.fr>
2024-01-29 22:04:08 +0100

    tentative to use fluidsynth; failed

--- compo.sh
+++ compo.sh
@@ -8,6 +8,10 @@ export TERM="xterm-256color"
 	qjackctl -s &
 	sleep 10
 	qsynth &
+#	fluidsynth -C0 -R0 -a jack -m jack /home/pi/soundfonts/00_FluidR3_GM.sf2 &
+#	jack_connect fluidsynth-midi:left system:playback_1
+#	jack_connect fluidsynth-midi:right system:playback_2
 	sleep 5
 	x-terminal-emulator -e /home/pi/compo.a
+
 ) >& $LOGFILE
