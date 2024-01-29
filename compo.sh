433c2420ca21c76d6a00694ecb8a27b7e7f4bc14
denybear <98181634+denybear@users.noreply.github.com>
2024-01-29 12:41:30 +0100

    debug

--- compo.sh
+++ compo.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-sleep 5
-qjackctl -s &
-sleep 10
-qsynth &
-sleep 5
-./compo.a
+
+LOGFILE=/home/pi/log
+export TERM="xterm-256color"
+
+(
+	sleep 5
+	qjackctl -s &
+	sleep 10
+	qsynth &
+	sleep 5
+	/home/pi/compo.a
+) >& $LOGFILE
